030433ad1ab3b4af546e059c5db75b6bfb8eb3be
Tate Capra <tatecapra@outlook.com>
2020-06-10 16:36:18 -0500

    Update to index js

--- src/index.js
+++ src/index.js
@@ -6,30 +6,71 @@ import './index.css';
 // create components
 
 class Square extends React.Compnent {
-  constructor(props) {
-    super(props);
-    this.state ={
-      value: null,
-    };
-  }
 
   render() {
     return(
         <button
         className="square"
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick()}
         >
         {this.props.value}
         </button>
     );
   }
 }
+
 class Board extends React.Compnent {
+  constructor(props){
+    super(props);
+    this.state = {
+      square: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i){
-    return <Square value={i} />;
+    return (
+      <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+  render() {
+    const = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
   }
 }
 
 class Game extends React.Compnent {
-
+  render(){
+    return (
+      
+    )
+  }
 }
